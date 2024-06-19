7c0f238ad7019124b8e2a2c7f07c67284e8091cc
B. Manishkumar <112232600+manish-kumar-3@users.noreply.github.com>
2024-06-19 06:52:17 +0530

    Update 12.early parser.py

--- 12.early parser.py
+++ 12.early parser.py
@@ -1,31 +1,36 @@
-
 def earley_parser(grammar, start_symbol, input_string):
     chart = [[] for _ in range(len(input_string) + 1)]
-    chart[0].append(('S', (start_symbol,), 0))
+    start_state = (start_symbol, (start_symbol,), 0, 0)
+    chart[0].append(start_state)
 
     for i in range(len(input_string) + 1):
         for state in chart[i]:
-            if state[2] < len(state[1]) and state[1][state[2]] in grammar:
-                non_term = state[1][state[2]]
-                for rule in grammar[non_term]:
-                    new_state = (non_term, rule, 0)
-                    if new_state not in chart[i]:
-                        chart[i].append(new_state)
-
-            elif state[2] < len(state[1]) and state[1][state[2]] == input_string[i - 1]:
-                new_state = (state[0], state[1], state[2] + 1)
-                if new_state not in chart[i]:
-                    chart[i].append(new_state)
-
-            elif state[2] == len(state[1]):
-                for s in chart[state[3]]:
-                    if s[2] < len(s[1]) and s[1][s[2]] == state[0]:
-                        new_state = (s[0], s[1], s[2] + 1)
+            lhs, rhs, dot, origin = state
+            if dot < len(rhs):
+                next_symbol = rhs[dot]
+                if next_symbol in grammar:
+                    # Predictor
+                    for rule in grammar[next_symbol]:
+                        new_state = (next_symbol, rule, 0, i)
+                        if new_state not in chart[i]:
+                            chart[i].append(new_state)
+                elif i < len(input_string) and next_symbol == input_string[i]:
+                    # Scanner
+                    new_state = (lhs, rhs, dot + 1, origin)
+                    if new_state not in chart[i + 1]:
+                        chart[i + 1].append(new_state)
+            else:
+                # Completer
+                for s in chart[origin]:
+                    s_lhs, s_rhs, s_dot, s_origin = s
+                    if s_dot < len(s_rhs) and s_rhs[s_dot] == lhs:
+                        new_state = (s_lhs, s_rhs, s_dot + 1, s_origin)
                         if new_state not in chart[i]:
                             chart[i].append(new_state)
 
     for state in chart[-1]:
-        if state[0] == 'S' and state[2] == len(state[1]) and state[3] == 0:
+        lhs, rhs, dot, origin = state
+        if lhs == start_symbol and dot == len(rhs) and origin == 0:
             return True
     return False
 
@@ -35,10 +40,10 @@ def main():
         'NP': [('Det', 'N'), ('NP', 'PP')],
         'VP': [('V', 'NP'), ('VP', 'PP')],
         'PP': [('P', 'NP')],
-        'Det': ['the', 'a'],
-        'N': ['man', 'dog', 'cat'],
-        'V': ['chased', 'sat'],
-        'P': ['on', 'in']
+        'Det': [('the',), ('a',)],
+        'N': [('man',), ('dog',), ('cat',)],
+        'V': [('chased',), ('sat',)],
+        'P': [('on',), ('in',)]
     }
     start_symbol = 'S'
     input_string = input("Enter a string: ").strip().split()
